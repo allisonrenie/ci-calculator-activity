5de82477f4b0be9368862037be4da7d5b517c6ea
allisonrenie <allisonrenie@gmail.com>
2021-06-06 21:46:56 -0700

    adding test file

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+"""
+tests for calc app
+"""
+
+import calculator
+
+class TestCalcApp:
+    def test_add(self):
+        assert 7 == calculator.add(3, 4)
+
+
+    def test_subtract(self):
+        assert 7 == calculator.subtract(14, 7)
