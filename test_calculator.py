a5f2bcc469dbb6ffe42b6db1d9a3f9a090613a12
allisonrenie <allisonrenie@gmail.com>
2021-06-06 22:16:51 -0700

    style changes to test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,10 @@ tests for calc app
 
 import calculator
 
+
 class TestCalcApp:
     def test_add(self):
         assert 7 == calculator.add(3, 4)
-
 
     def test_subtract(self):
         assert 7 == calculator.subtract(14, 7)
